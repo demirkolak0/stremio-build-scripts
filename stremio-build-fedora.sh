aae8d92b2cbd8bb2b94c6034c3f980adef358103
Demir Kolak <demirkolak8@gmail.com>
2026-07-08 14:11:54 +0300

    Add missing dependencies installation in script

--- stremio-build-fedora.sh
+++ stremio-build-fedora.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+sudo dnf install nodejs wget librsvg2-devel librsvg2-tools mpv-libs-devel qt5-qtbase-devel qt5-qtwebengine-devel qt5-qtquickcontrols qt5-qtquickcontrols2 openssl-devel gcc g++ make glibc-devel kernel-headers binutils -y
 git clone --recurse-submodules https://github.com/Stremio/stremio-shell.git
 cd stremio-shell
 sed -i 's/qmake/qmake-qt5/g' release.makefile
