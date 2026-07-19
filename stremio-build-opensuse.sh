6384f03a3ed456adf29c18c12a697c73d2a7f00c
Demir Kolak <demirkolak8@gmail.com>
2026-07-19 16:26:14 +0300

    Update stremio-build-opensuse.sh

--- stremio-build-opensuse.sh
+++ stremio-build-opensuse.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
-zypper install -y nodejs wget librsvg-devel rsvg-convert mpv-devel libqt5-qtbase-devel libqt5-qtwebengine-devel libqt5-qtquickcontrols libqt5-qtquickcontrols2 libopenssl-devel gcc gcc-c++ make glibc-devel kernel-devel binutils
+zypper install -y nodejs wget librsvg-devel rsvg-convert mpv mpv-devel libqt5-qtbase-devel libqt5-qtwebengine-devel libqt5-qtquickcontrols libqt5-qtquickcontrols2 libopenssl-devel gcc gcc-c++ make glibc-devel kernel-devel binutils
 git clone --recurse-submodules https://github.com/Stremio/stremio-shell.git
 cd stremio-shell
 sed -i 's/qmake/qmake-qt5/g' release.makefile
 qmake-qt5
-make -f release.makefile
+make -f release.makefile install
 ./dist-utils/common/postinstall
 cd ..
 echo "DONE!!"
